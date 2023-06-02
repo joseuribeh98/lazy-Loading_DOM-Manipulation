b5b2265b0b2f38d2200b986878adf2109e21390c
Jose Uribe <joseuribeh@gmail.com>
2023-06-01 22:30:37 -0500

    Basic Layout & JS logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const API = 'https://randomfox.ca/images'
+const appContainer = document.getElementById("app");
+const max = 122
+const min = 1
+const random = () => Math.floor (Math.random() * (max-min)) + min
+const addButton = document.querySelector('#addButton')
 
-console.log('Happy hacking :)')
+const createImgNode = (urlApi) => {
+  const imgDiv = document.createElement("div");
+  imgDiv.className = "p-4";
+
+  const img = document.createElement("img");
+  img.className = "mx-auto";
+  img.width = "320";
+  img.src = `${urlApi}/${random()}.jpg`
+
+  imgDiv.appendChild(img);
+
+  return imgDiv
+};
+
+const createImg = () => {
+    const newImg = createImgNode(API)
+    appContainer.appendChild(newImg);
+}
+
+addButton.addEventListener('click', createImg)
